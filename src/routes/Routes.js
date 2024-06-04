04fe22d4fb2197545ddb6ac2ef31c9acc9e0218d
Sofia Ximenes <sofiaantonacio@gmail.com>
2024-06-04 14:20:27 -0300

    inluindo elementos página pokemon

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../screens";
+import { Pokemon } from "../screens";
 
 
 const AppRoutes = () => {
     return (
             <Routes>
-                <Route  path="/home" element={<Home />} />
+                <Route  path="/" element={<Home />} />
+                <Route  path="/pokemon/:id" element={<Pokemon />} />
             </Routes>
     );
 };
